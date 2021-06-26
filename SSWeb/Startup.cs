0e885dc5fdf5217f9221752a092184893d0a6255
Tuyen 'Tyrus' Ng <trgtuyen@gmail.com>
2021-06-26 22:18:50 +0700

    Push Area

--- SSWeb/Startup.cs
+++ SSWeb/Startup.cs
@@ -52,6 +52,13 @@ namespace SSWeb
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
